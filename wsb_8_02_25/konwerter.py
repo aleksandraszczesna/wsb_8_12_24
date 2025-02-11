7e7ffefd8382b5c66ed73dd62ecc05b1817f1d74
AS <aszczesna98@gmail.com>
2025-02-11 17:44:13 +0100

    wsb_8_02_25

---
+++ wsb_8_02_25/konwerter.py
@@ -0,0 +1,5 @@
+def km_na_mile(km):
+    return km * 0.621371
+
+def mile_na_km(mila):
+    return mila * 1.60934
---
+++ wsb_8_02_25/moj_modul.py
@@ -0,0 +1,9 @@
+def przywitaj(imie):
+    print(f"Cześć {imie}! Miło Cię poznać.")    #Funkcja witająca z podanym imieniem
+
+def policz_do_trzech():
+    for i in range(1,4):    #Funkcja wypisująca kolejne liczby od 1 do 3
+        print(i)
+
+wiadomosc_powitalna = "Witaj w moim module!"
+
